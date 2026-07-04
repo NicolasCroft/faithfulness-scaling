70ab3abec29a20f4301e1a4f9d3a7134575beb7b
Nick <ncroft@stanford.edu>
2026-07-04 13:43:25 -0400

    Add pipeline package init

---
+++ pipeline/__init__.py
@@ -0,0 +1,8 @@
+"""Faithfulness-scaling pipeline package.
+
+Modules:
+    data         - loading and filtering the math problem eval set
+    corruptions  - CoT corruption methods (truncate / substitute / remove)
+    inference    - model backend abstraction (mock + hosted API stubs)
+    scoring      - faithfulness rate computation and confidence intervals
+"""
